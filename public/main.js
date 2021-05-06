8da7bfc8b22b38e6a6024d9cce31b95add182213
amy <amyjojjm2001@gmail.com>
2021-05-06 01:22:13 -0700

    basic styling

--- public/main.js
+++ public/main.js
@@ -1,6 +1,9 @@
-const updateBtn = document.querySelector('#update-button')
 
-updateBtn.addEventListener('click', _ => {
+const update = document.querySelector('#update-button')
+const deleteButton = document.querySelector('#delete-button')
+const messageDiv = document.querySelector('#message')
+
+update.addEventListener('click', _ => {
     fetch('/quotes', {
         method: 'put',
         headers: { 'Content-Type': 'application/json' },
@@ -9,4 +12,31 @@ updateBtn.addEventListener('click', _ => {
             quote: 'And someday when the descendants of humanity have spread from star to star they won’t tell the children about the history of Ancient Earth until they’re old enough to bear it and when they learn they’ll weep to hear that such a thing as Death had ever once existed'
         })
     })
+        .then(res => {
+            if (res.ok) return res.json()
+        })
+        .then(response => {
+            window.location.reload(true)
+        })
+})
+
+deleteButton.addEventListener('click', _ => {
+    fetch('/quotes', {
+        method: 'delete',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            name: 'Harry Potter'
+        })
+    })
+        .then(res => {
+            if (res.ok) return res.json()
+        })
+        .then(response => {
+            if (response === 'No quote to delete') {
+                messageDiv.textContent = 'No Dumbledore or Voldemort quote to delete'
+            } else {
+                window.location.reload(true)
+            }
+        })
+        .catch(console.error)
 })
--- server.js
+++ server.js
@@ -8,66 +8,77 @@ const connectionString = 'mongodb+srv://adminQuote:mongo712;@cluster0.nnlhd.mong
 
 const port = process.env.PORT || 3000
 
-
-
+    
 MongoClient.connect(connectionString, { useUnifiedTopology: true })
-    .then(client => {
-        console.log('Connected to Database')
-        const db = client.db('hp-quotes')
-        const quotesCollection = db.collection('quotes')
+.then(client => {
+  console.log('Connected to Database')
+  const db = client.db('hp-quotes')
+  const quotesCollection = db.collection('quotes')
 
+  // Middlewares
+  app.set('view engine', 'ejs')
+  app.use(bodyParser.urlencoded({ extended: true }))
+  app.use(bodyParser.json())
+  app.use(express.static('public'))
 
-        app.use(bodyParser.urlencoded({ extended: true }))
-        app.set('view engine', 'ejs')
-        app.use(express.static('public'))
-        app.use(bodyParser.json())
+  // ========================
+  // Routes
+  // ========================
+  app.get('/', (req, res) => {
+    db.collection('quotes').find().toArray()
+      .then(quotes => {
+        res.render('index.ejs', { quotes: quotes })
+      })
+      .catch(error => console.error(error))
+  })
 
+  app.post('/quotes', (req, res) => {
+    quotesCollection.insertOne(req.body)
+      .then(result => {
+        res.redirect('/')
+      })
+      .catch(error => console.error(error))
+  })
 
-        app.post('/quotes', (req, res) => {
-            quotesCollection.insertOne(req.body)
-                .then(result => {
-                    res.redirect('/')
-                })
-                .catch(error => console.error(error))
-        })
+  app.put('/quotes', (req, res) => {
+    quotesCollection.findOneAndUpdate(
+      { name: 'Dumbledore' },
+      {
+        $set: {
+          name: req.body.name,
+          quote: req.body.quote
+        }
+      },
+      {
+        upsert: true
+      }
+    )
+      .then(result => res.json('Success'))
+      .catch(error => console.error(error))
+  })
 
-        app.get('/', (req, res) => {
-            db.collection('quotes').find().toArray()
-                .then(results => {
-                    // console.log(results)
-                    // res.sendFile('/Users/admin/Downloads/beforeGit/crud-quotes' + '/index.html')
-                    res.render('index.ejs', { quotes: results })
-                })
-                .catch(error => console.error(error))
+  app.delete('/quotes', (req, res) => {
+    quotesCollection.deleteOne(
+      { name: req.body.name }
+    )
+      .then(result => {
+        if (result.deletedCount === 0) {
+          return res.json('No quote to delete')
+        }
+        res.json('Deleted Harry Potter\'s quote')
+      })
+      .catch(error => console.error(error))
+  })
 
-        })
+  // ========================
+  // Listen
+  // ========================
+  const isProduction = process.env.NODE_ENV === 'production'
+  const port = isProduction ? 7500 : 3000
+  app.listen(port, function () {
+    console.log(`listening on ${port}`)
+  })
+})
+.catch(console.error)
 
-        app.put('/quotes', (req, res) => {
-            console.log(req.body)
-            // quotesCollection.findOneAndUpdate(
-            //     { name: 'Harry Potter' },
-            //     {
-            //       $set: {
-            //         name: req.body.name,
-            //         quote: req.body.quote
-            //       }
-            //     },
-            //     {
-            //       upsert: true
-            //     }
-            //   )
-            //   .then(result => {
-            //     console.log(result)
-            //    })
-            //     .catch(error => console.error(error))
-        })
-
-
-        app.listen(port, () => {
-            console.log('Server is up on port ' + port)
-        })
-
-    })
-    .catch(error => console.error(error))
-
-
+  
